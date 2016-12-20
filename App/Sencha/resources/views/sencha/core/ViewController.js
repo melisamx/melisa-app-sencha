c5de16f036a399c24439c8706c139a779704ad61
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-12-20 10:52:40 -0600

    The use of the existing moduleRun function in the ViewController

--- App/Sencha/resources/views/sencha/core/ViewController.js
+++ App/Sencha/resources/views/sencha/core/ViewController.js
@@ -1,6 +1,10 @@
 Ext.define('Melisa.core.ViewController', {
     extend: 'Ext.app.ViewController',
     
+    requires: [
+        'Melisa.core.module.Manager'
+    ],
+    
     mixins: !Ext.platformTags.classic ? [
         'Melisa.core.Base',
         'Melisa.ux.Navigate'
@@ -37,6 +41,47 @@ Ext.define('Melisa.core.ViewController', {
     activateModule: function(module) {
         
         Ext.GlobalEvents.fireEvent('activatemodule', module);
+        
+    },
+    
+    moduleRun: function(config, callbackOnReady, callbackOnReboot, params, scope) {
+        
+        var me = this,
+            options = {
+                single: true
+            };
+        
+        if( !Ext.isFunction(callbackOnReady)) {
+            
+            callbackOnReady = Ext.emptyFn;
+            
+        }
+        
+        if( !Ext.isFunction(callbackOnReboot)) {
+            
+            callbackOnReboot = Ext.emptyFn;
+            
+        }
+        
+        if( params) {
+            
+            options.args = Ext.isArray(params) ? params : [ params ];
+            
+        }
+                
+        Melisa.core.module.Manager.launch(config, function(module) {
+            
+            if( module.getIsReady()) {
+                
+                module.on('reboot', callbackOnReady, scope | me, options);
+                
+            } else {
+                
+                module.on('ready', callbackOnReboot, scope | me, options);
+                
+            }
+            
+        });
         
     }
         
