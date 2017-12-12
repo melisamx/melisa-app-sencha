e971b6e2a10b665bd2edb9b1376077e92c4a10d1
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-12-12 13:08:25 -0600

    Se ajusto no cerrar ventana al presional tecla ESC.

--- App/Sencha/resources/views/sencha/override/window/Window.js
+++ App/Sencha/resources/views/sencha/override/window/Window.js
@@ -1,6 +1,18 @@
 Ext.define('Melisa.override.window.Window', {
     override: 'Ext.window.Window',
     
-    bodyPadding: '15 12'
+    bodyPadding: '15 12',
+    
+    onEsc: function(e) {
+        var me = this;
+        e.stopEvent();
+//        Ext.Msg.confirm('Closing confirmation', 'YOU REALLY WANTS TO close',
+//            function(btn) {
+//                if (btn === 'yes') {
+//                    me.close();
+//                }
+//            }
+//        );
+    }
     
 });
